35e54287b50097c44385d3fc435dd6cb0c945853
ahmed-khalid2004 <ahmadhamadakhalid16@gmail.com>
2025-05-02 01:06:44 +0300

    Part 08 - Employee Module - Delete Action

--- DataAccess/Repositories/Classes/GenericRepository.cs
+++ DataAccess/Repositories/Classes/GenericRepository.cs
@@ -10,8 +10,8 @@ namespace MVC03.DataAccess.Repositories.Classes
 
         public IEnumerable<TEntity> GetAll(bool WithTracking = false)
         {
-            if (WithTracking) return _dbContext.Set<TEntity>().ToList();
-            else return _dbContext.Set<TEntity>().AsNoTracking().ToList();
+            if (WithTracking) return _dbContext.Set<TEntity>().Where(E => E.IsDeleted != true).ToList();
+            else return _dbContext.Set<TEntity>().Where(E => E.IsDeleted != true).AsNoTracking().ToList();
         }
 
 
